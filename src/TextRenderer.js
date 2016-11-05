463f879e01666cea71d1404fb3f06bd8c4f5227d
Devon Govett <devongovett@gmail.com>
2016-11-04 20:37:29 -0700

    Update TextRenderer

--- src/TextRenderer.js
+++ src/TextRenderer.js
@@ -1,6 +1,9 @@
 export default class TextRenderer {
-  constructor(ctx) {
+  constructor(ctx, options = {}) {
     this.ctx = ctx;
+    this.outlineBlocks = options.outlineBlocks || false;
+    this.outlineLines = options.outlineLines || false;
+    this.outlineRuns = options.outlineRuns || false;
   }
 
   render(container) {
@@ -10,18 +13,39 @@ export default class TextRenderer {
   }
 
   renderBlock(block) {
+    if (this.outlineBlocks) {
+      this.ctx.rect(block.bbox.minX, block.bbox.minY, block.bbox.width, block.bbox.height).stroke();
+    }
+
     for (let line of block.lines) {
       this.renderLine(line);
     }
   }
 
   renderLine(line) {
-    for (let run of block.runs) {
+    if (this.outlineLines) {
+      this.ctx.rect(line.rect.x, line.rect.y, line.rect.width, line.rect.height).stroke();
+    }
+
+    this.ctx.save();
+    this.ctx.translate(line.rect.x, line.rect.maxY);
+    this.ctx.scale(1, -1, {});
+
+    for (let run of line.runs) {
       this.renderRun(run);
     }
+
+    this.ctx.restore();
   }
 
   renderRun(run) {
-    this.ctx.renderGlyphs(run.glyphs, run.positions);
+    if (this.outlineRuns) {
+      this.ctx.rect(0, 0, run.advanceWidth, run.height).stroke();
+    }
+
+    for (let i = 0; i < run.run.glyphs.length; i++) {
+      run.run.glyphs[i].render(this.ctx, run.attributes.fontSize);
+      this.ctx.translate(run.run.positions[i].xAdvance * run.scale, 0)
+    }
   }
 }
--- temp.js
+++ temp.js
@@ -5,6 +5,7 @@ import LineFragmentGenerator from './src/LineFragmentGenerator';
 import Rect from './src/geom/Rect';
 import LayoutEngine from './src/LayoutEngine';
 import AttributedString from './src/models/AttributedString';
+import TextRenderer from './src/TextRenderer';
 
 let path = new Path;
 path.circle(150, 150, 120);
@@ -44,7 +45,7 @@ doc.stroke('red');
 let string = AttributedString.fromFragments([
   {
     string: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ',
-    attributes: {font: 'Helvetica', bold: true, fontSize: 14}
+    attributes: {font: 'Hoefler Text', bold: true, fontSize: 14}
   },
   {
     string: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
@@ -57,24 +58,9 @@ console.log(string)
 let l = new LayoutEngine;
 let block = l.layoutParagraph(string, path, [exclusion, exclusion.scale(0.5).translate(100, 220)]);
 
+let renderer = new TextRenderer(doc, {outlineBlocks: true});
+renderer.renderBlock(block);
+
 console.log(block)
-
-for (let line of block.lines) {
-  // doc.rect(line.rect.x, line.rect.y, line.rect.width, line.rect.height).stroke('purple');
-
-  doc.save();
-  doc.translate(line.rect.x, line.rect.maxY);
-  doc.scale(1, -1, {});
-
-  let x = line.rect.x;
-  for (let run of line.runs) {
-    for (let i = 0; i < run.run.glyphs.length; i++) {
-      run.run.glyphs[i].render(doc, run.attributes.fontSize);
-      doc.translate(run.run.positions[i].xAdvance * run.scale, 0)
-    }
-  }
-
-  doc.restore();
-}
 
 doc.end();
