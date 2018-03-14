7d3f6670bef2df422ca4fc03a7f114f9c4bd7271
Diego Muracciole <diegomuracciole@gmail.com>
2018-03-14 11:22:47 -0300

    Add some more

---
+++ test/Attachment.test.js
@@ -0,0 +1,29 @@
+import Attachment from '../src/models/Attachment';
+
+describe('Attachment', () => {
+  test('should handle passed width', () => {
+    const attachment = new Attachment(5);
+
+    expect(attachment).toHaveProperty('width', 5);
+  });
+
+  test('should handle passed height', () => {
+    const attachment = new Attachment(5, 10);
+
+    expect(attachment).toHaveProperty('height', 10);
+  });
+
+  test('should handle passed image', () => {
+    const opts = { image: 'blah' };
+    const attachment = new Attachment(5, 10, opts);
+
+    expect(attachment).toHaveProperty('image', opts.image);
+  });
+
+  test('should handle passed render', () => {
+    const opts = { render: 'blah' };
+    const attachment = new Attachment(5, 10, opts);
+
+    expect(attachment).toHaveProperty('render', opts.render);
+  });
+});
