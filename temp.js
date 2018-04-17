5f4321b1859ee1622d94a23d8dea0fa028a0530b
Diego Muracciole <diegomuracciole@gmail.com>
2018-04-17 13:59:23 -0300

    Support font objects with name in TextRenderer

--- temp.js
+++ temp.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import PDFDocument from 'pdfkit';
+import FontSubstitutionEngine from 'font-substitution';
 import { Path, LayoutEngine, AttributedString, Container, TextRenderer } from './src';
 
 const path = new Path();
@@ -22,32 +23,32 @@ const string = AttributedString.fromFragments([
   {
     string: 'Lorem ipsum dolor sit amet, ',
     attributes: {
-      font: 'Arial',
-      fontSize: 14,
+      font: 'Helvetica',
+      fontSize: 10,
       bold: true,
       align: 'justify',
       hyphenationFactor: 0.9,
       hangingPunctuation: true,
       lineSpacing: 5,
-      lineHeight: 24,
+      // lineHeight: 24,
       truncate: true
     }
   },
   {
     string: 'consectetur adipiscing elit, ',
     attributes: {
-      font: 'Arial',
-      fontSize: 18,
+      font: 'Courier',
+      fontSize: 12,
       color: 'red',
       align: 'justify'
     }
   },
   {
     string:
-      'sed 🎉 do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea volupt\u0301ate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?”',
+      'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea volupt\u0301ate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?”',
     attributes: {
-      font: 'Comic Sans MS',
-      fontSize: 14,
+      font: 'Helvetica',
+      fontSize: 10,
       align: 'justify',
       hyphenationFactor: 0.9,
       hangingPunctuation: true,
@@ -57,7 +58,9 @@ const string = AttributedString.fromFragments([
   }
 ]);
 
-const l = new LayoutEngine();
+const l = new LayoutEngine({
+  fontSubstitutionEngine: new FontSubstitutionEngine()
+});
 const container = new Container(path, {
   exclusionPaths: [exclusion]
 });
