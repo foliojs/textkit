9c2ca4d0034b2ecd527928d1be1165dc872ee582
Diego Muracciole <diegomuracciole@gmail.com>
2018-04-29 19:09:34 -0300

    Fix LayoutEngine tests

--- packages/core/test/layout/LayoutEngine.test.js
+++ packages/core/test/layout/LayoutEngine.test.js
@@ -1,11 +1,12 @@
 import LayoutEngine from '../../src/layout/LayoutEngine';
 
 class TestEngine {}
+const createEngine = () => TestEngine;
 
 describe('LayoutEngine', () => {
   test('should be able to inject custom lineBreaker engine', () => {
     const layout = new LayoutEngine({
-      lineBreaker: new TestEngine()
+      lineBreaker: createEngine
     });
 
     expect(layout.typesetter.lineBreaker.constructor.name).toBe(TestEngine.name);
@@ -13,7 +14,7 @@ describe('LayoutEngine', () => {
 
   test('should be able to inject custom lineFragmentGenerator engine', () => {
     const layout = new LayoutEngine({
-      lineFragmentGenerator: new TestEngine()
+      lineFragmentGenerator: createEngine
     });
 
     expect(layout.typesetter.lineFragmentGenerator.constructor.name).toBe(TestEngine.name);
@@ -21,7 +22,7 @@ describe('LayoutEngine', () => {
 
   test('should be able to inject custom justificationEngine engine', () => {
     const layout = new LayoutEngine({
-      justificationEngine: new TestEngine()
+      justificationEngine: createEngine
     });
 
     expect(layout.typesetter.justificationEngine.constructor.name).toBe(TestEngine.name);
@@ -29,7 +30,7 @@ describe('LayoutEngine', () => {
 
   test('should be able to inject custom truncationEngine engine', () => {
     const layout = new LayoutEngine({
-      truncationEngine: new TestEngine()
+      truncationEngine: createEngine
     });
 
     expect(layout.typesetter.truncationEngine.constructor.name).toBe(TestEngine.name);
@@ -37,7 +38,7 @@ describe('LayoutEngine', () => {
 
   test('should be able to inject custom decorationEngine engine', () => {
     const layout = new LayoutEngine({
-      decorationEngine: new TestEngine()
+      decorationEngine: createEngine
     });
 
     expect(layout.typesetter.decorationEngine.constructor.name).toBe(TestEngine.name);
@@ -45,7 +46,7 @@ describe('LayoutEngine', () => {
 
   test('should be able to inject custom tabEngine engine', () => {
     const layout = new LayoutEngine({
-      tabEngine: new TestEngine()
+      tabEngine: createEngine
     });
 
     expect(layout.typesetter.tabEngine.constructor.name).toBe(TestEngine.name);
